c95ee58a5216875050aad846d84995fcd19fff47
cqm3ron <camerondbissell@gmail.com>
2024-10-22 21:34:32 +0100

    change to html (third language change lmao)

--- Form1.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form1
-    Private Sub btnAddTitle_Click(sender As Object, e As EventArgs) Handles btnAddTitle.Click
-        Dim newOval As New OvalControl()
-        newOval.Text = "Sample text that wraps inside the oval"
-        newOval.Font = New Font("Arial", 10)
-        newOval.Location = New Point(50, 50)
-        Me.Controls.Add(newOval)
-    End Sub
-End Class
